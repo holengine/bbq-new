e9414437aa0556b4dc0c03f29a420ada17854b18
hol3ng <savinyhal@gmail.com>
2023-01-26 17:58:53 +0300

    refactor event_mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: event.user.email, subject: "#{t('.title')} @ #{event.title}"
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий @ #{event.title}"
+    mail to: email, subject: "#{t('.title')} @ #{event.title}"
   end
 
   def photo(event, photo, email)
     @event = event
     @photo = photo
 
-    mail to: email, subject: "Новое фото в @ #{event.title}"
+    mail to: email, subject: "#{t('.title')} @ #{event.title}"
   end
 end
