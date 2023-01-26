6b9942c15d5acbe13a396a8ac30f6506fc9a4fc9
hol3ng <savinyhal@gmail.com>
2023-01-26 18:13:10 +0300

    user does not receive mail from his comment and photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -44,6 +44,7 @@ class PhotosController < ApplicationController
 
   def notify_subscribers(event, photo)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails.delete(photo.user.email)
 
     all_emails.each do |mail|
       EventMailer.photo(event, photo, mail).deliver_now
