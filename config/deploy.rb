d2f95dbffe204ce21cd8bf4af3c9c00bea12c2da
hol3ng <savinyhal@gmail.com>
2023-01-27 16:14:07 +0300

    toggle require_master_key

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,23 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock '~> 3.17.1'
 
-set :application, "bbq"
-set :repo_url, "git@github.com:holengine/bbq-new.git"
+set :application, 'bbq'
+set :repo_url, 'git@github.com:holengine/bbq-new.git'
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/www/bbq"
+set :deploy_to, '/home/deploy/www/bbq'
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, 'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
 
 after 'deploy:restart', 'resque:restart'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, "main"
+set :branch, 'main'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
